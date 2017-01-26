9e0a10f2dbacbca47561c9b96476260ad041d087
FrTim1908 <FrTim1908@PC_FRTIM1908>
2017-01-27 00:09:33 +0100

    Created shiplist and movement for different ships. Different shiplengths.

--- Game/Main/game.py
+++ Game/Main/game.py
@@ -14,24 +14,24 @@ class Game:
         self.ship6 = ships.Ship(globals.green, 4, 2,1,2,2,self.board,4,3)
         self.ship7 = ships.Ship(globals.green, 4, 2,1,2,2,self.board,4,3)
         self.ship8 = ships.Ship(globals.green, 4, 2,1,2,2,self.board,4,3)
-        
+        self.shiplist = [self.ship1,self.ship2,self.ship3,self.ship4,self.ship5,self.ship6,self.ship7,self.ship8]
 
     def game_loop(self):
         time.sleep(0.1)
-        s1 = True
-        s2 = False
+
         pause_text = pygame.font.SysFont('freesansbold.ttf', 50).render('Paused', True, globals.white)
         RUNNING, PAUSE = 0, 1
         state = RUNNING
 
         gameExit = False
-        gameExit2 = False
+
         globals.gameDisplay.fill(globals.black)
         self.board.draw()
         pygame.display.update()
-        shiplength1 = 5
-        shiplength2 = 3
-        while not gameExit and self.shipsplaced <2:
+        shipcnt = 0
+        shiplength = 0
+
+        while not gameExit and self.shipsplaced <4:
         
 
             for event in pygame.event.get():
@@ -46,20 +46,22 @@ class Game:
                 click = pygame.mouse.get_pressed()
             
                 if click[0] == 1:
-                    #print("schip plaatsen")
+                    time.sleep(0.2)
                     mouse = pygame.mouse.get_pos()
-                    
-                    #print(mousex)
                     if self.shipsplaced == 0:
-                        mousex1 = int(mouse[0]/20)
-                        shipy1 = 21 - shiplength1
-                        self.ship1  = ships.Ship(globals.green, shiplength1, mousex1,shipy1, 2, 1, self.board, 1,3)
-                        self.ship1.draw()
-                    if self.shipsplaced == 1:
-                        mousex2 = int(mouse[0]/20)
-                        shipy2 = 21 - shiplength2
-                        self.ship2  = ships.Ship(globals.green, shiplength2, mousex2,shipy2, 2, 1, self.board, 1,3)
-                        self.ship2.draw()
+                        shiplength = 2
+                    elif self.shipsplaced == 1 or self.shipsplaced == 2:
+                        shiplength = 3
+                    else:
+                        shiplength = 4
+
+                    mousex1 = int(mouse[0]/20)
+                    self.shiplist[self.shipsplaced].PosX = mousex1
+                    self.shiplist[self.shipsplaced].ShipLength = shiplength
+                    self.shiplist[self.shipsplaced].PosY = 21 - shiplength
+                    self.shiplist[self.shipsplaced].draw()
+
+
                     self.shipsplaced +=1
                     time.sleep(0.2)              
                 
@@ -75,9 +77,10 @@ class Game:
             self.board.draw()
             pygame.display.update()
             globals.clock.tick(60)
+            
 
-
-        while self.shipsplaced == 2:
+        while self.shipsplaced == 4:
+            #time.sleep(5)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -86,60 +89,39 @@ class Game:
                 #Pause key
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        if s1 and not s2:
-                            s1 = False
-                            s2 = True
-                        elif not s1 and s2:
-                            s1 = True
-                            s2 = False
+                        if shipcnt < 3:
+                           shipcnt += 1
+                        else:
+                            shipcnt = 0
+
                         
                     if event.key == pygame.K_p:
                         state = PAUSE
                     if event.key == pygame.K_u:
                         state = RUNNING
 
-                    if s1 == True:
-                        if event.key == pygame.K_LEFT:
-                            if mousex1 == 1: 
-                                mousex1 += 0
-                            else:
-                                mousex1 += -1
-                        if event.key == pygame.K_RIGHT:
-                            if mousex1 == 20:
-                                mousex1 += 0
-                            else:
-                                mousex1 += 1
-                        if event.key == pygame.K_UP:
-                            if shipy1 == 1:
-                                shipy1 += 0
-                            else:
-                               shipy1 += -1
-                        if event.key == pygame.K_DOWN:
-                            if shipy1 == (21 -shiplength1):
-                                shipy1 += 0
-                            else:
-                                shipy1 += 1
-                    if s2 == True:
-                        if event.key == pygame.K_LEFT:
-                            if mousex2 == 1: 
-                                mousex2 += 0
-                            else:
-                                mousex2 += -1
-                        if event.key == pygame.K_RIGHT:
-                            if mousex2 == 20:
-                                mousex2 += 0
-                            else:
-                                mousex2 += 1
-                        if event.key == pygame.K_UP:
-                            if shipy2 == 1:
-                                shipy2 += 0
-                            else:
-                               shipy2 += -1
-                        if event.key == pygame.K_DOWN:
-                            if shipy2 == (21 -shiplength2):
-                                shipy2 += 0
-                            else:
-                                shipy2 += 1                            
+                    
+                    if event.key == pygame.K_LEFT:
+                        if self.shiplist[shipcnt].PosX == 1: 
+                            self.shiplist[shipcnt].PosX += 0
+                        else:
+                            self.shiplist[shipcnt].PosX += -1
+                    if event.key == pygame.K_RIGHT:
+                        if self.shiplist[shipcnt].PosX == 20:
+                            self.shiplist[shipcnt].PosX += 0
+                        else:
+                            self.shiplist[shipcnt].PosX += 1
+                    if event.key == pygame.K_UP:
+                        if self.shiplist[shipcnt].PosY == 1:
+                            self.shiplist[shipcnt].PosY += 0
+                        else:
+                            self.shiplist[shipcnt].PosY += -1
+                    if event.key == pygame.K_DOWN:
+                        if self.shiplist[shipcnt].PosY == (21 - self.shiplist[shipcnt].ShipLength):
+                            self.shiplist[shipcnt].PosY += 0
+                        else:
+                            self.shiplist[shipcnt].PosY += 1
+                          
 
                 while state == PAUSE:
                     globals.gameDisplay.blit(pause_text,(300,300))
@@ -148,14 +130,23 @@ class Game:
                     for event in pygame.event.get():
                         if event.type == pygame.KEYDOWN:
                             if event.key == pygame.K_u: state = RUNNING
-                 
+                  
 
             globals.gameDisplay.fill(globals.black)
             self.board = playboard.Grid(globals.gameDisplay, globals.white,1)
-            self.ship1  = ships.Ship(globals.green, shiplength1, mousex1,shipy1, 2, 1, self.board, 1,3)
-            self.ship2  = ships.Ship(globals.green, shiplength2, mousex2,shipy2, 2, 1, self.board, 1,3)
+            for x in range(0,8):
+                self.shiplist[x].Board = self.board
+
+            self.ship1 = self.shiplist[0]
+            self.ship2 = self.shiplist[1]
+            self.ship3 = self.shiplist[2]
+            self.ship4 = self.shiplist[3]
+
+
             self.ship1.draw()
             self.ship2.draw()
+            self.ship3.draw()
+            self.ship4.draw()
             self.board.draw()
         
             pygame.display.update()
