7ea9e76951d4e8a244dbb313057f3f43de0df0ac
Fabian Lallemand <0931771@hr.nl>
2017-01-31 23:34:52 +0100

    moved music to main

--- Game/Main/main.py
+++ Game/Main/main.py
@@ -3,10 +3,15 @@ PyGame Battleport
 Fabian Lallemand, Tim van Leeuwen, Bob Verkaik & Damian van Vuuren
 """
 
-import pygame, menu
+import pygame, menu, globals
+
+
 
 pygame.init()
 pygame.display.set_caption('Battleport')
+
+pygame.mixer.init()
+globals.MenuSoundfx.play().set_volume(0.2)
     
 menu.game_intro()
 pygame.quit()
