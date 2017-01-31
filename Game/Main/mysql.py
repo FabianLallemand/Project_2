20c878b4a3bedbe9060ca6c50d17dba9f6b511ce
Bob Verkaik <0891805@hr.nl>
2017-01-31 16:40:17 +0100

    Fixed highscores, update works

--- Game/Main/mysql.py
+++ Game/Main/mysql.py
@@ -2,45 +2,32 @@ import pymysql
 
 
 class mysql:
+    def __init__(self, array = False):
+        if not array:
+	        #set connection types
+            self.connection = pymysql.connect(
+	            host='localhost',
+	            user='battleport',
+	            password='ditiseengeheim',
+	            db='highscores',
+	            cursorclass=pymysql.cursors.DictCursor
+            )
 
-	def __init__(self, array = False):
-		if not array:
-			#set connection types
-			self.connection = pymysql.connect(
-				host='localhost',
-				user='battleport',
-				password='ditiseengeheim',
-				db='highscores',
-				cursorclass=pymysql.cursors.DictCursor
-			)
+    def select(self, query):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+            finally:
+                self.connection.close()
+                return result
 
-	def select(self, query):
-		with self.connection.cursor() as cursor:
-			try:
-				cursor.execute(query)
-				result = cursor.fetchall()
-			finally:
-				self.connection.close()
-				return result
+    def update(self, query):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                self.connection.commit()
+            finally:
+                self.connection.close()
+                return True
 
-	def insert(self, query):
-		self.__alter_table(query)
-
-	def update(self, query):
-		self.__alter_table(query)
-
-	def delete(self, query):
-		self.__alter_table(query)
-
-	def __alter_table(self, query):
-		with self.connection.cursor() as cursor:
-			try:
-				with self.connection.cursor as cursor:
-					cursor.execute(query)
-
-				self.connection.commit()
-			except:
-				self.connection.rollback()
-			finally:
-				self.connection.close()
-				return True
