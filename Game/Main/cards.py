60f9f6c2be2bf9cf010e4069cb1f44b04c1f7180
Damian van Vuuren <0905098@hr.nl>
2017-01-31 11:51:23 +0100

    cards added

--- Game/Main/cards.py
+++ Game/Main/cards.py
@@ -1,8 +1,31 @@
 import pygame
 
 class Cards:
-    def __init__(self, name):
+    def __init__(self, name,action):
         self.Name = name
+        self.Action = action
 
-card1 = Cards("FMJ Upgrade")
-card2 = Cards("Rifling")
+#offensieve kaarten
+card1 = Cards("FMJ Upgrade",None)
+card2 = Cards("Rifling",None)
+card3 = Cards("Adv Rifling",None)
+card4 = Cards("Naval Mine",None)
+card5 = Cards("EMP Upgrade",None)
+#defensieve kaarten
+card6 = Cards("Hull",None)
+card7 = Cards("Sonar",None)
+card8 = Cards("Smokescreen",None)
+card9 = Cards("Sabotage",None)
+#Helpende kaarten
+card10 = Cards("Backup",None)
+card11 = Cards("Extrafuel2",None)
+card12 = Cards("Extrafuel",None)
+card13 = Cards("Rally",None)
+card14 = Cards("Adr. Rush",None)
+#speciale kaarten
+card15 = Cards("Repair",None)
+card16 = Cards("Flak Armor",None)
+card17 = Cards("Hack Intel",None)
+card18 = Cards("Far Sight",None)
+card19 = Cards("Allu. Hull",None)
+card20 = Cards("J. Sparrow",None)
