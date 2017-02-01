b3971221f4e33f540bcd63a39801cf1a183e56f9
Fabian Lallemand <0931771@hr.nl>
2017-02-01 16:15:30 +0100

    Make settings great again 2.0

--- Game/Main/settings.py
+++ Game/Main/settings.py
@@ -13,24 +13,23 @@ def settings():
                 quit()
 
         #Background code
-        globals.gameDisplay.blit(globals.BackgroundBlur, (0,0))
+        globals.gameDisplay.blit(globals.BackgroundBlur,(0,0))
         
         TextSurf, TextRect = text.text_objects("Credits", globals.largeText, globals.bright_grey)
         TextRect.center = ((400),(55)) 
         globals.gameDisplay.blit(TextSurf, TextRect)
         
 
-
         text.button("Back!",690,10,100,50,globals.grey,globals.bright_red,menu.game_intro)
-        text.button("Music ON",350,150,100,50,globals.grey,globals.bright_red,lambda: music("start"))
-        text.button("Music OFF",350,200,100,50,globals.grey,globals.bright_red,lambda: music("stop"))
+        text.button("Music",350,100,100,50,globals.grey,globals.bright_red,lambda: music("start"))
+        text.button("Music",350,175,100,50,globals.grey,globals.bright_red, lambda: music("stop"))
 
         mouse = pygame.mouse.get_pos()
         pygame.display.update()
         globals.clock.tick(60)
 
-    def music(state):
-        if state == "start":
-            globals.MenuSoundfx.play
-        elif state == "stop":
-            globals.MenuSoundfx.stop
+def music(state):
+    if state == "start":
+        globals.MenuSoundfx.play()
+    elif state == "stop":
+        globals.MenuSoundfx.stop()
