f6b6c4001b3d2e5b1d78ffdac5af814f4d6e6930
Fabian Lallemand <0931771@hr.nl>
2017-02-01 15:52:11 +0100

    make settings great again

--- Game/Main/settings.py
+++ Game/Main/settings.py
@@ -13,17 +13,24 @@ def settings():
                 quit()
 
         #Background code
-        BackGround = pygame.image.load('assets/background.jpg')
+        globals.gameDisplay.blit(globals.BackgroundBlur, (0,0))
         
         TextSurf, TextRect = text.text_objects("Credits", globals.largeText, globals.bright_grey)
         TextRect.center = ((400),(55)) 
         globals.gameDisplay.blit(TextSurf, TextRect)
         
-        globals.gameDisplay.blit(BackGround, (0,0))
+
 
         text.button("Back!",690,10,100,50,globals.grey,globals.bright_red,menu.game_intro)
-        text.button("Music",350,50,100,50,globals.grey,globals.bright_red,None)
+        text.button("Music ON",350,150,100,50,globals.grey,globals.bright_red,lambda: music("start"))
+        text.button("Music OFF",350,200,100,50,globals.grey,globals.bright_red,lambda: music("stop"))
 
         mouse = pygame.mouse.get_pos()
         pygame.display.update()
         globals.clock.tick(60)
+
+    def music(state):
+        if state == "start":
+            globals.MenuSoundfx.play
+        elif state == "stop":
+            globals.MenuSoundfx.stop
